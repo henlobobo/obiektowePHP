232893e7e8da66e7d95b63a4d5ef67bd459b987d
henlobobo <91115583+henlobobo@users.noreply.github.com>
2021-09-29 14:55:16 +0200

    Update php2.php

--- php2.php
+++ php2.php
@@ -1,12 +1,17 @@
 <?php
 if(!empty($_POST)){
-    $liczba=$_POST["liczba"];
+
+        
+   // $liczba=$_POST["liczba"];
+    /*
     if($liczba<=0){
         echo'Nie można tak' .$liczba;
     }
     else{
         echo sqrt($liczba);
     }
-}
 
+*/
+ echo ($_POST["liczba"]<=0) ?   'Nie można tak' .$_POST["liczba"] :  sqrt($_POST["liczba"]);
+}
 ?>
